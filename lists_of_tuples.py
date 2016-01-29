7cb80a7419b99af41cb7f92b300ce7dd8e4305b3
mitacDUO <steelea@student.unimelb.edu.au>
2016-01-29 19:18:33 +1100

    python problems

--- lists_of_tuples.py
+++ lists_of_tuples.py
@@ -1,5 +1,11 @@
-def ListsFromTuples(x):
+def ListsFromTuples():
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     alpha = list(alphabet)
     return [(alpha[x],alpha.index(alpha[x])+1) for x in range(26)]
-    
+
+def TwoLists():
+    one,two = map(list,zip(*ListsFromTuples()))
+    three = one,two
+    return list(three)
+
+print(TwoLists())
